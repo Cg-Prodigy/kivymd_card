6bf34cdda3dc12e30878592507d5c77e5bead2ff
Cg-Prodigy <brightmarshall2@gmail.com>
2021-09-15 13:58:34 +0300

    MDCard Implementation

---
+++ app/main_ui.py
@@ -0,0 +1,6 @@
+from configparser import InterpolationMissingOptionError
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivy.clock import Clock
+
+class FloatBuild(MDFloatLayout):
+    pass
